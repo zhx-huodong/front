0f93f15931620ae794f82f41a202ae76209a923f
luhaixing <2062531131@qq.com>
2020-03-13 15:36:41 +0800

    打包

--- src/tools/config.js
+++ src/tools/config.js
@@ -1,7 +1,8 @@
 let protocol = location.protocol;
 let devHost = 'api.huodong.eduinspector.com';
 const isLocal = location.hostname === 'huodong.eduinspector.com' || location.hostname === 'localhost';
-let host = isLocal ? devHost : location.host;
+// let host = isLocal ? devHost : location.host;
+let host=devHost
 
 let config = {
 	protocol: protocol,
