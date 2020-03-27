97451b078097ab5a005478528d356ee366f3a242
luhaixing <2062531131@qq.com>
2020-03-27 15:07:05 +0800

    修改

---
+++ src/pages/homePages/WorksPreview.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="works-preview">
+    <div class="title">
+      <p>{{activityDetail.works.title}}</p>
+      <span style="position:absolute;right:100px;color:#999999;font-size:12px">作品编号:{{activityDetail.serial_id}}</span>
+    </div>
+    <el-divider></el-divider>
+    <el-row>
+      <el-col :span="20" :offset="2">
+        <div class="content-main">
+          <div class="content-item">
+              <div class="sub-title">创作思想</div>
+              <div class="content" v-html="activityDetail.works.content.content">
+           
+              </div>
+          </div>
+          <div class="content-item">
+              <div class="sub-title">创作过程</div>
+              <div class="content">
+                  {{activityDetail.works.content.production}}
+              </div>
+          </div>
+          <div class="content-item">
+              <div class="sub-title">参考资源</div>
+              <div class="content">
+                  {{activityDetail.works.content.reference}}           
+              </div>
+          </div>
+          <div class="content-item">
+              <div class="sub-title">制作用软件及运行环境</div>
+              <div class="content">{{activityDetail.works.content.environment}}             
+              </div>
+          </div>
+          <div class="content-item">
+              <div class="sub-title">其他说明</div>
+              <div class="content">{{activityDetail.works.content.remark}}      
+              </div>
+          </div>
+          
+          <div class="content-item"  style="width:50%">
+              <div class="sub-title">作品:</div>
+              <div class="content" v-for="(item,index) in activityDetail.works.attachment" :key="index">
+                  <file-preview :fileObj="item"></file-preview>      
+              </div>
+          </div>
+          <div class="content-item">
+              <div class="sub-title">报名登记表:</div>
+              <div class="content" >
+                  <file-preview :fileObj="{name:'报名登记表',url:activityDetail.registration}"></file-preview>      
+              </div>
+          </div>
+          <el-divider></el-divider>
+          <div class="works-detail-main">
+              <div class="works-detail-item">
+                  活动组别：{{activityDetail.info.activity}}
+              </div>
+              <div class="works-detail-item">
+                  活动项目：{{activityDetail.info.project}}
+              </div>
+              <div class="works-detail-item">
+                  学段：{{periodList[activityDetail.period]}}
+              </div>
+              <div class="works-detail-item">
+                  学校：{{activityDetail.school.title}}
+              </div>
+              <div class="works-detail-item">
+                  作者：{{activityDetail.works.member.author.map(item=>{return item.name+'('+item.mobile+')'}).join('、')}}
+              </div>
+              <div class="works-detail-item">
+                  指导老师：{{activityDetail.works.member.mentor.map(item=>{return item.name+'('+item.mobile+')'}).join('、')}}
+              </div>
+              <div class="works-detail-item">
+                  邮箱：{{activityDetail.works.email}}
+              </div>
+          </div>
+
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+import api from "../../service/api.js";
+import { getCookie } from "../../tools/tools";
+import FilePreview from "../../components/FilePreview";
+export default {
+  components: { FilePreview },
+  props: {
+    id: {
+      type: Number,
+      default() {
+        return 0;
+      }
+    }
+  },
+
+  data() {
+    return {
+        activityDetail:{},//报名详情信息
+        periodList: { 1: "幼教组" ,2: "小学组", 4: "初中组", 8: "高中组",  16: "特教组", 32: "中职组", 64: "高教组" }
+
+    };
+  },
+  mounted() {
+      this.$nextTick(()=>{
+          this.getActivityDetail()
+      })
+  },
+  methods: {
+      //获取报名活动详情
+    async getActivityDetail() {
+      let params = {};
+      params.url = api.enroll;
+      params.id = this.id;
+      params.expand = "info,works,school,professional,award,process";
+      await this.axiosGet(params)
+        .then(res => {
+          this.activityDetail = res;
+        })
+        .catch(err => err);
+    },
+  }
+};
+</script>
+<style lang="less" scoped>
+.works-preview {
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+  .content-main{
+      .content-item{
+          display: flex;
+          flex-direction: column;
+          margin-bottom: 30px;
+          .sub-title{
+              font-size: 16px;
+              color: #333;
+          }
+          .content{
+              margin-top: 10px;
+              font-size: 14px;
+              color: #666;
+              line-height: 30px;
+          }
+      }
+  }
+  .works-detail-main{
+      display: flex;
+      flex-direction: column;
+      
+      .works-detail-item{
+          font-size: 14px;
+          color: #333;
+            margin-bottom: 10px;
+      }
+  }
+}
+</style>
--- src/pages/homePages/activityDetail.vue
+++ src/pages/homePages/activityDetail.vue
@@ -1,128 +1,121 @@
 <template>
-    <div class="activity-container">
-        <el-card>
-            <div class="title">
-                <p>{{activityDetail.info.activity}}</p>
-            </div>
-            <el-divider></el-divider>
-            <el-steps :active="process" align-center>
-                <el-step title="作品报名" :description="formatDateChar(activityDetail.info.nodes[0].stime*1000)+'--'+formatDateChar(activityDetail.info.nodes[0].etime*1000)"></el-step>
-                <el-step title="区域推荐" :description="formatDateChar(activityDetail.info.nodes[1].stime*1000)+'--'+formatDateChar(activityDetail.info.nodes[1].etime*1000)"></el-step>
-                <el-step title="市级评审" :description="formatDateChar(activityDetail.info.nodes[2].stime*1000)+'--'+formatDateChar(activityDetail.info.nodes[2].etime*1000)"></el-step>
-                <el-step title="作品展示" :description="formatDateChar(activityDetail.info.nodes[3].stime*1000)+'--'+formatDateChar(activityDetail.info.nodes[2].etime*1000)"></el-step>
-            </el-steps>  
-        </el-card>
-        <el-card style="margin-top:10px;">
-            <div class="title">
-                <p>{{activityDetail.works.title}}</p>
-                <span style="position:absolute;right:0;color:#999999">作品编号:{{activityDetail.serial_id}}</span>
-                <div class="act-button">
-                    <!-- v-show="activityDetail.period<=1" -->
-                    <el-button type="primary"  plain size="small" @click="goToEdit" v-if="look!=0&&process==1">修改</el-button>
-                </div>
-            </div>
-            <el-divider></el-divider>
-            <div class="content-main">
-                <div  v-html="activityDetail.works.content"></div>
-                <!-- <el-image :src="activityDetail.works.cover" fit="contain"></el-image> -->
-                <el-form ref="form" :model="form" label-width="90px" style="margin-top:50px;">
-                    <el-form-item label="活动组别：">
-                        {{activityDetail.info.category}}
-                    </el-form-item>
-                    <el-form-item label="活动项目：">
-                        {{activityDetail.info.project}}
-                    </el-form-item>
-                    <el-form-item label="作者：">
-                        <template v-for="item in activityDetail.works.member.author">{{item.name}}、</template>
-                    </el-form-item>
-                    <el-form-item label="指导老师：">
-                        <template v-for="item in activityDetail.works.member.mentor">{{item.name}}、</template>
-                    </el-form-item>
-                    <el-form-item label="作者邮箱：">
-                       {{activityDetail.works.email}}
-                    </el-form-item>
-                </el-form>
-            </div>
-        </el-card>
-    </div>
+  <div class="activity-container">
+    <el-card>
+      <div class="title">
+        <p>{{activityDetail.info.activity}}</p>
+      </div>
+      <el-divider></el-divider>
+      <el-steps :active="process" align-center>
+        <el-step
+          title="作品报名"
+          :description="formatDateChar(activityDetail.info.nodes[0].stime*1000)+'--'+formatDateChar(activityDetail.info.nodes[0].etime*1000)"
+        ></el-step>
+        <el-step
+          title="区域推荐"
+          :description="formatDateChar(activityDetail.info.nodes[1].stime*1000)+'--'+formatDateChar(activityDetail.info.nodes[1].etime*1000)"
+        ></el-step>
+        <el-step
+          title="市级评审"
+          :description="formatDateChar(activityDetail.info.nodes[2].stime*1000)+'--'+formatDateChar(activityDetail.info.nodes[2].etime*1000)"
+        ></el-step>
+        <el-step
+          title="作品展示"
+          :description="formatDateChar(activityDetail.info.nodes[3].stime*1000)+'--'+formatDateChar(activityDetail.info.nodes[2].etime*1000)"
+        ></el-step>
+      </el-steps>
+    </el-card>
+    <el-card style="margin-top:10px;">
+        <div class="act-button">
+          <el-button
+            type="primary"
+            plain
+            size="small"
+            @click="goToEdit"
+            v-if="look!=0&&process==1"
+          >修改</el-button>
+        </div>
+        <works-preview :id="id"></works-preview>
+    </el-card>
+  </div>
 </template>
 <script>
-    import api from "../../service/api.js";
-    export default{
-        data(){
-            return {
-                imgUrl:require('../../public/images/ac1.png'),
-                form:{},
-                id:this.$route.query.id,//获取详情id
-                activityDetail:{},//活动详情
-                look:this.$route.query.look,
-                process:1,//活动状态
-            }
-        },
-        mounted(){
-
-            this.getActivityDetail()
-        },
-        methods:{
-            //获取报名活动详情
-            async getActivityDetail(){
-                let params={}
-                params.url=api.enroll
-                params.id=this.id
-                params.expand="info,works,school,professional,award,process"
-                await this.axiosGet(params).then(res=>{
-                    this.activityDetail=res
-                    let nowTime=Date.parse(new Date())
-                    if((res.info.nodes[0].stime*1000)<=nowTime<=(res.info.nodes[0].etime*1000)){
-                        this.process=1
-                    }else if((res.info.nodes[1].stime*1000)<=nowTime<=(res.info.nodes[1].etime*1000)){
-                        this.process=2
-                    }else if((res.info.nodes[2].stime*1000)<=nowTime<=(res.info.nodes[2].etime*1000)){
-                        this.process=3
-                    }else if((res.info.nodes[3].stime*1000)<=nowTime<=(res.info.nodes[3].etime*1000)){
-                        this.process=4
-                    }
-                }).catch(err=>err)
-            },
-            //修改
-            goToEdit(){
-                let that=this;
-                this.$router.push({
-                    path:'/home/activityEnroll',
-                    query:{id:that.id,activityName:that.activityDetail.info.category}
-                })
-            },
-        }
+import api from "../../service/api.js";
+import WorksPreview from './WorksPreview'
+export default {
+    components:{WorksPreview},
+  data() {
+    return {
+      imgUrl: require("../../public/images/ac1.png"),
+      form: {},
+      id: this.$route.query.id, //获取详情id
+      activityDetail: {}, //活动详情
+      look: this.$route.query.look,
+      process: 1 //活动状态
+    };
+  },
+  mounted() {
+    this.getActivityDetail();
+  },
+  methods: {
+    //获取报名活动详情
+    async getActivityDetail() {
+      let params = {};
+      params.url = api.enroll;
+      params.id = this.id;
+      params.expand = "info,works,school,professional,award,process";
+      await this.axiosGet(params)
+        .then(res => {
+          this.activityDetail = res;
+          let nowTime = Date.parse(new Date());
+          if (
+            res.info.nodes[0].stime * 1000 <=
+            nowTime <=
+            res.info.nodes[0].etime * 1000
+          ) {
+            this.process = 1;
+          } else if (
+            res.info.nodes[1].stime * 1000 <=
+            nowTime <=
+            res.info.nodes[1].etime * 1000
+          ) {
+            this.process = 2;
+          } else if (
+            res.info.nodes[2].stime * 1000 <=
+            nowTime <=
+            res.info.nodes[2].etime * 1000
+          ) {
+            this.process = 3;
+          } else if (
+            res.info.nodes[3].stime * 1000 <=
+            nowTime <=
+            res.info.nodes[3].etime * 1000
+          ) {
+            this.process = 4;
+          }
+        })
+        .catch(err => err);
+    },
+    //修改
+    goToEdit() {
+      let that = this;
+      this.$router.push({
+        path: "/home/activityEnroll",
+        query: { id: that.id, activityName: that.activityDetail.info.category }
+      });
     }
+  }
+};
 </script>
 <style lang="less" scoped>
-.activity-container{
-    width:1180px;
-    margin:auto;
-    margin-top:20px;
-    .title{
-        display:flex;
-        justify-content:center;
-        position:relative;
-        .act-button{
-            position:absolute;
-            right:0px;
-        }
+.activity-container {
+  width: 1180px;
+  margin: auto;
+  margin-top: 20px;
+  position: relative;
+    .act-button {
+      position: absolute;
+      right: 20px;
     }
-    .content-main{
-        width:800px;
-        margin:auto;
-        p{
-            margin-top:30px;
-            font-size:14px;
-            line-height:30px;
-            text-indent:2em;
-        }
-        .el-image{
-            margin-top:30px;
-            // width:800px;
-            // height:400px;
-        }
-    }
+ 
 }
 </style>
